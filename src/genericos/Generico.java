cf6dc3c6156e29d75afa781dc103c8317af447d3
Brin29 <breinerstivenparracortes50@gmail.com>
2024-09-13 11:16:22 -0500

    Genericos

---
+++ src/genericos/Generico.java
@@ -0,0 +1,26 @@
+package genericos;
+
+// Creacion del generico
+// Tipos acotados que tipo debe ser la clase
+public class Generico<T extends Number> {
+		
+	private final T tipoObjeto;
+	
+	public Generico(T tipoObjeto) {
+		this.tipoObjeto = tipoObjeto;
+	}
+	
+	public void show() {
+		System.out.println("Esta es la clase del generico" + tipoObjeto.getClass());
+	}
+	
+	// Generico en clase
+	static <T> void mostrar(T tipoObjeto) {
+		System.out.println("Esta es la clase del generico" + tipoObjeto.getClass());
+	}
+	
+	// Mas de uno
+	static <T, D> void mostrarMasDeUno(T tipoObjeto, D segundoObjeto) {
+		System.out.println("Esta es la clase del generico" + tipoObjeto.getClass() + segundoObjeto.getClass());
+	}
+}
---
+++ src/genericos/genericosPractice.java
@@ -0,0 +1,38 @@
+package genericos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class genericosPractice {
+
+	public static void main(String[] args) {
+		
+		// Los genericos permiten definir que tipo de valor se puede ingresar
+		
+		// Como no se define el tipo de lista se castea
+		//List lista = new ArrayList<>();
+		//lista.add("Hola");
+		
+		//final String ejemplo = (String) lista.get(0);
+		
+		// Ya no se castea
+		List<String> lista = new ArrayList<>();
+		lista.add("Hola");
+		lista.add("Adios");
+		
+		final String ejemplo = lista.get(0);	
+		
+		// Generico creado
+		Generico<Double> generico = new Generico(1.5);
+		
+		generico.show();
+		
+		// String no hereda de nombre
+		//Generico<String> genericoString = new Generico("Hola Mundo");
+		//generico.show();
+		
+		// Tipos acotados que tipo debe ser la clase
+		
+		Generico.mostrar(1);
+	}
+}
